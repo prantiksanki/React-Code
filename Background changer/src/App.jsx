f9db92e67fa6fe3dfaab2e4ba0bc49e45b6d3631
Prantik Sanki <prantiksanki@gmail.com>
2025-01-08 23:32:36 +0530

    Project Upload

---
+++ Background changer/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  function background(color)
+  { 
+     document.body.style.backgroundColor = color; 
+  }
+
+  return (
+    <>
+    <div style={{padding: "10px" , alignItems: "self-end" , margin: "10px"}}>
+    <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" style = {{backgroundColor : "green"}} onClick={() =>background("green")}>Green</button>
+      <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" style = {{backgroundColor : "blue"}}  onClick={() =>background("blue")}>Blue</button>
+      <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" style = {{backgroundColor : "red"}} onClick={() =>background("red")}>Red</button>
+      <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" style = {{backgroundColor : "yellow" , color : "black"}} onClick={() => background("yellow")}>Yellow</button>
+      <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" style = {{backgroundColor : "black" , color :"white"}} onClick={() =>background("black")}>Black</button>
+      <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" style = {{backgroundColor : "white" , color : "black"}} onClick={() =>background("white")}>White</button>
+
+    </div>
+    
+    </>
+  )
+}
+
+export default App
