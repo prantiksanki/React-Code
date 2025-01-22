bb9163b829d01d7810c0a4be3a56b29f126d9372
Prantik Sanki <prantiksanki@gmail.com>
2025-01-22 19:29:57 +0530

    Minute update

--- Background changer/src/App.jsx
+++ Background changer/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" style = {{backgroundColor : "white" , color : "black"}} onClick={() =>background("white")}>White</button>
 
     </div>
-    
+        
     </>
   )
 }
